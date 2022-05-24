17752a6d403d6e69a661f2bf659ec5dd5305e402
RekiDunois <rekidunois+github@gmail.com>
2022-05-24 11:58:35 +0800

    keymap: add system clipboard keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,4 +1,6 @@
 local map=require('utils').map
+local xmap=require('utils').xmap
+local nmap=require('utils').nmap
 
 -- I love wasd
 map('','a','h')
@@ -17,3 +19,9 @@ map('i','jj','<ESC>')
 -- nvim-tree
 -- map('n','tt',':NvimTreeToggle<CR>') NvimTreeFocus can switch focus betwen editor and tree panel
 -- map('n','tt',':NvimTreeFocus<CR>')
+
+xmap("<C-y>", [["+y]])
+nmap("<C-v>", [["+p]])
+map("i", "<C-v>", [[<ESC>"+pa]])
+
+nmap("<C-z>", "u")
