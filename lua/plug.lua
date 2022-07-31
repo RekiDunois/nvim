cd0f824046f7cfdbd2cb0652d359e0966865d2c4
RekiDunois <rekidunois+github@gmail.com>
2022-07-31 20:16:16 +0800

    plug: rm nvim-tree, add fm

--- lua/plug.lua
+++ lua/plug.lua
@@ -3,13 +3,7 @@ require('packer').init{
 }
 
 return require('packer').startup(function(use)
-	use {
-    		'kyazdani42/nvim-tree.lua',
-    		requires = {
-      			'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    		},
-    		config = function() require'config.nvimtree' end
-		}
 	use {'gcmt/wildfire.vim', event = "BufRead"}
 	use {'mtdl9/vim-log-highlighting'}
+	use {'is0n/fm-nvim'}
 end)
