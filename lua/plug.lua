0f038a6c8669041ec6123196a0880a4657f361f7
RekiDunois <rekidunois+github@gmail.com>
2022-08-05 11:00:43 +0800

    plug: add nvim-tree config via lua file

--- lua/plug.lua
+++ lua/plug.lua
@@ -3,6 +3,19 @@ require('packer').init{
 }
 
 return require('packer').startup(function(use)
+	use {
+    		"plasticboy/vim-markdown",
+    		ft = {
+      			"markdown",
+    		},
+	}
+	use {
+  		'kyazdani42/nvim-tree.lua',
+		config = function() require("config.nvimtree") end,
+  		requires = { 
+			'kyazdani42/nvim-web-devicons', -- optional, for file icons
+		},
+	}
 	use {'gcmt/wildfire.vim', event = "BufRead"}
 	use {'mtdl9/vim-log-highlighting'}
 	use {'is0n/fm-nvim'}
