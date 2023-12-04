2a331fd137817cc09e16edbbb8b2e493866a7145
RekiDunois <rekidunois+github@gmail.com>
2023-12-04 17:04:22 +0800

    chore: formating

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,28 +1,28 @@
-local map=require('utils').map
-local xmap=require('utils').xmap
-local nmap=require('utils').nmap
-local fmap=require('utils').fmap
+local map = require('utils').map
+local xmap = require('utils').xmap
+local nmap = require('utils').nmap
+local fmap = require('utils').fmap
 
 -- I love wasd
-map('','a','h')
-map('','s','j')
-map('','d','l')
-map('','w','k')
-map('','j','5k')
-map('','m','n')
-map('','M','N')
-map('','n','5j')
-map('','A','^')
-map('','D','g_')
-map('','h','b')
-map('','l','w')
-map('','k','a')
+map('', 'a', 'h')
+map('', 's', 'j')
+map('', 'd', 'l')
+map('', 'w', 'k')
+map('', 'j', '5k')
+map('', 'm', 'n')
+map('', 'M', 'N')
+map('', 'n', '5j')
+map('', 'A', '^')
+map('', 'D', 'g_')
+map('', 'h', 'b')
+map('', 'l', 'w')
+map('', 'k', 'a')
 nmap('<C-j>', '<C-w>k')
 nmap('<C-n>', '<C-w>j')
 nmap('<C-l>', '<C-w>l')
 nmap('<C-h>', '<C-w>h')
 
-map('i','jj','<ESC>')
+map('i', 'jj', '<ESC>')
 
 -- copy and pase to system
 xmap("<C-c>", [["+y]])
@@ -37,5 +37,4 @@ nmap("<C-s>", ":w<ENTER>")
 -- tabpage
 nmap("<S-Tab>", ":tabprevious<CR>")
 nmap("<Tab>", ":tabnext<CR>")
-nmap("<C-t>", ":tabnew<CR>")
-
+nmap("<S-t>", ":tabnew<CR>")
